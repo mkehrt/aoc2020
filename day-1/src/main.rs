5c7b4a3a7f30cf4e0f888dee62dd0330598e702e
Matthew Kehrt <mkehrt@roseandsigil.net>
2020-12-02 21:15:25 -0800

    1.1

---
+++ day-1/src/main.rs
@@ -0,0 +1,21 @@
+use std::{env, fs::File, path::Path, io::{BufRead, BufReader}};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = Path::new(&args[1]);
+    let file = File::open(input).unwrap();
+    let reader = BufReader::new(file);
+    let contents: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+    let length = contents.len();
+    'outer: for i in 0..length {
+        for j in i..length {
+           let int: i32 = contents[i].parse::<i32>().unwrap();
+           let jnt: i32 = contents[j].parse::<i32>().unwrap();
+           if int + jnt == 2020 {
+               let output = int * jnt;
+               println!("{}", output);
+               break 'outer;
+           }
+        }
+    }
+}
