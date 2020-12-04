9888fe9240acc3fa51041a11e5ea5bbb5edafe1b
Matthew Kehrt <mkehrt@roseandsigil.net>
2020-12-04 00:14:31 -0800

    2.2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -17,14 +17,9 @@ fn main() {
     let mut count = 0;
     for line in lines {
         let (lower, upper, char, pwd) =
-            scan_fmt!(&line.unwrap(), "{}-{} {}: {}", i32, i32, String, String).unwrap();
-        let mut char_count = 0;
-        for pwd_char in pwd.chars() {
-            if pwd_char.to_string() == char {
-                char_count += 1;
-            }
-        }
-        if lower <= char_count && char_count <= upper {
+            scan_fmt!(&line.unwrap(), "{}-{} {}: {}", usize, usize, String, String).unwrap();
+        let chars: Vec<char> = pwd.chars().collect();
+        if (chars[lower - 1].to_string() == char) ^ (chars[upper - 1].to_string() == char) {
             count += 1;
         }
     }
