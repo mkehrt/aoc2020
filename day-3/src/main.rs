2a68180bad146c79d120c99ad75ea1fed9c932c4
Matthew Kehrt <mkehrt@roseandsigil.net>
2020-12-04 19:19:31 -0800

    3.2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -10,17 +10,26 @@ fn main() {
     let input = Path::new(&args[1]);
     let file = File::open(input).unwrap();
     let reader = BufReader::new(file);
-    let lines = reader.lines();
-    let mut count = 0;
-    let mut x = 0;
-    for line in lines {
-        let chars: Vec<char> = line.unwrap().chars().collect();
-        let width = chars.len();
-        let char = chars[x % width];
-        if char.to_string() == "#" {
-            count += 1;
+    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+    let mut product: i64 = 1;
+    let length = lines.len();
+    for (dx, dy) in &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] {
+        let mut count = 0;
+        let mut x = 0;
+        let mut y = 0;
+        while y < length {
+            let line = &lines[y];
+            let chars: Vec<char> = line.chars().collect();
+            let width = chars.len();
+            let char = chars[x % width];
+            if char.to_string() == "#" {
+                count += 1;
+            }
+            x += dx;
+            y += dy;
         }
-        x += 3;
+        println!("{}", count);
+        product *= count;
     }
-    println!("{}", count);
+    println!("{}", product);
 }
