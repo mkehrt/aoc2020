4d1520436d957ceacbf4843eb9215bc6ec5f0b9f
Matthew Kehrt <mkehrt@roseandsigil.net>
2020-12-08 00:42:13 -0800

    6.2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,4 +1,13 @@
-use std::{env, fs::read_to_string, path::Path, collections::HashSet};
+use std::{cmp::Eq, collections::HashSet, env, fs::read_to_string, hash::Hash, path::Path};
+
+fn optional_intersection<T: Eq + Hash + Copy>(
+    optional_left: Option<HashSet<T>>,
+    right: HashSet<T>,
+) -> Option<HashSet<T>> {
+    optional_left
+        .map(|left| left.intersection(&right).map(|x| *x).collect())
+        .or(Some(right))
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,17 +15,24 @@ fn main() {
     let contents = read_to_string(path).unwrap();
 
     let groups = contents.split("\n\n");
-    let mut total= 0;
+    let mut total = 0;
 
     for group in groups {
-        let mut chars = HashSet::new();
-        for char in group.chars() {
-            if char >= 'a' && char <= 'z' {
+        println!("Group: {}", group);
+        let mut common_chars: Option<HashSet<char>> = None;
+        // This only works if the file is newline terminated for some reason...
+        let lines = group.split("\n");
+        for line in lines {
+            let mut chars = HashSet::new();
+            for char in line.chars() {
                 chars.insert(char);
             }
+            common_chars = optional_intersection(common_chars, chars);
+            println!("Common: {:?}", common_chars.clone().unwrap());
         }
-        total+= chars.len();
+        println!("");
+        total += common_chars.unwrap().len();
     }
 
-    println!("{}",total);
+    println!("{}", total);
 }
